45a81559854fc477ff78345280faf097db130618
77d2wn4b5y-dev <77d2wn4b5y@privaterelay.appleid.com>
2026-07-25 00:03:18 +0300

    Release version 2.7.1

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME="trebovanje-v266-customers";
+const CACHE_NAME="trebovanje-v271-settings";
 const APP_FILES=["./","./index.html","./styles.css","./product-ui.css","./backup.css","./security.css","./supabase-sync.css","./order-ui-v211.css","./route-v23.css","./customer-module.css","./version-24.css","./settings-ios.css","./statistics.css","./price-lists.css","./dashboard.css","./app.js","./file-export.js","./report.js","./routes.js","./enhancements.js","./customers.js","./customer-module.js","./product-ui.js","./backup.js","./security.js","./supabase-sync.js","./order-ui-v211.js","./route-v23.js","./dashboard.js","./v24.js","./price-lists.js","./statistics.js","./settings-ios.js","./performance-v265.js","./manifest.webmanifest","./icon.svg"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_FILES)).then(()=>self.skipWaiting()));});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));});
